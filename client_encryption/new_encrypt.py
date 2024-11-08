be58df8fa81fdd8cba55db29140950b15b4505ed
Smrutiranjan-cloud <smrutiranjan92@gmail.com>
2024-11-08 16:50:30 +0000

    Update new_encrypt.py

--- client_encryption/new_encrypt.py
+++ client_encryption/new_encrypt.py
@@ -59,3 +59,33 @@ print("Encrypted Payload:", encrypted_payload)
 
 ################################
 ################################
+
+from mastercard_encryption import FieldLevelEncryption, FieldLevelEncryptionConfig
+
+# Create a FieldLevelEncryptionConfig instance
+config = FieldLevelEncryptionConfig()
+
+# Manually configure each required attribute
+config.encryption_path = "$.credit_card.number"  # Path for the field to be encrypted
+config.encrypted_value_field_path = "$.encrypted_data.encryptedValue"  # Path for placing the encrypted value
+config.encryption_certificate_path = "path/to/your_public_certificate.pem"  # Path to the public key certificate
+config.encryption_key_fingerprint = "your_encryption_key_fingerprint"  # Encryption key fingerprint
+config.decryption_path = "$.encrypted_data.encryptedValue"  # Path to find encrypted data for decryption
+config.decryption_key_path = "path/to/your_private_key.pem"  # Path to the private key for decryption
+config.oaep_padding_digest_algorithm = "SHA-512"  # Padding digest algorithm
+
+# Sample payload to be encrypted
+payload = {
+    "credit_card": {
+        "number": "5555555555554444",
+        "expiry": "12/23",
+        "cvv": "123"
+    },
+    "other_data": "sample_value"
+}
+
+# Encrypt the payload using FieldLevelEncryption
+encrypted_payload = FieldLevelEncryption.encrypt(payload, config)
+
+print("Encrypted Payload:", encrypted_payload)
+
